de1c27d30070de0f6d167b618651863dc793cdb3
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-10-14 10:40:27 -0400

    Update Photo_Gallery.py
    Removed Captions

--- Pages/Photo_Gallery.py
+++ Pages/Photo_Gallery.py
@@ -6,16 +6,16 @@ def photo_gallery_page():
     st.markdown("<h1 class='centered-title'>Explore Our Gallery</h1>", unsafe_allow_html=True)
 
     photos = [
-        {"caption": "Photo 1", "file": "NfzImages/FSG_Sign.JPG"},
-        {"caption": "Photo 2", "file": "NfzImages/NFZ_13U.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_13U_CHamps.JPG"},
-        {"caption": "Photo 4", "file": "NfzImages/NFZ_13U_Champs_2.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_13U_Coaches.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_at_FSG_1.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_Coachs_FSG_Champs.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_FSG_Team_Champs.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_FSG_Trophy.JPG"},
-        {"caption": "Photo 3", "file": "NfzImages/NFZ_Prayer_3.JPG"},
+        {"caption": " ", "file": "NfzImages/FSG_Sign.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_13U.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_13U_CHamps.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_13U_Champs_2.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_13U_Coaches.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_at_FSG_1.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_Coachs_FSG_Champs.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_FSG_Team_Champs.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_FSG_Trophy.JPG"},
+        {"caption": " ", "file": "NfzImages/NFZ_Prayer_3.JPG"},
         # Add more photos here
     ]
 
